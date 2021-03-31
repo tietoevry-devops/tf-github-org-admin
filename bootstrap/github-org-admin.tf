b4fa1a36559fa5828e20eafb34b1c780c3d13daa
Bjørge Solli <bjorge.solli@evry.com>
2021-03-31 14:17:56 +0200

    bump version

--- bootstrap/github-org-admin.tf
+++ bootstrap/github-org-admin.tf
@@ -1,5 +1,5 @@
 module "github-org-admin" {
-  source                             = "github.com/tietoevry-devops/tf-github-org-admin?ref=v0.1"
+  source                             = "github.com/tietoevry-devops/tf-github-org-admin?ref=v0.1.1"
   github_organization                = var.github_organization
   default_repo_settings              = var.default_repo_settings
   default_branch_protection_settings = var.default_branch_protection_settings
